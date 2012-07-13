f2d95e0782d5b405d4190d74f8da65264d5a5d0e
dmotz <motzdc@gmail.com>
2012-07-13 15:02:47 -0400

    fixed version num

--- bashcoin.js
+++ bashcoin.js
@@ -17,7 +17,7 @@ var https = require('https'),
     strikeouts = 0;
 
 bashcoin
-    .version('0.1')
+    .version('0.1.0')
     .option('-c, --cont', 'run continuously, query every 30 seconds')
     .option('-b, --buy', 'output buy')
     .option('-s, --sell', 'output sell')
